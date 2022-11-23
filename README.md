d3b09c94c17616e8b6556b186c04a65df2a04502
gemdoq <gemdoq@gmail.com>
2022-11-23 17:51:38 +0900

    fix(README.md and deploy.sh): fix

--- README.md
+++ README.md
@@ -3,14 +3,14 @@ Book Store
 ## EC2 접속주소
 
 ```shell
-ec2-43-201-101-29.ap-northeast-2.compute.amazonaws.com:8080/api/v1/books
+ec2-43-201-101-29.ap-northeast-2.compute.amazonaws.com:{port}/api/v1/books
 ```
 
 
 ## 배포
 
 ```shell
-sudo sh deploy.sh {db.url} {db.username} {db.password} {accessport} {imagename} {imageversion}
+sudo sh deploy.sh {db.url} {db.username} {db.password} {port} {imagename} {imageversion}
 ```
 
 ## Endpoint
--- deploy.sh
+++ deploy.sh
@@ -35,14 +35,29 @@ IMAGE=$(docker images --filter=reference="$NAME" -q)
 
 if [ -n "$IMAGE" ];
 then
+  echo "이미지 삭제"
+
 	docker rmi $IMAGE
+fi
+
+USING_PORT_ID=$(docker ps -aq --filter "expose=$PORT")
+
+if [ -n "$USING_PORT_ID" ];
+then
+  echo "포트번호 $PORT 컨테이너 종료"
+
+  docker stop $USING_PORT_ID
+
+  echo "포트번호 $PORT 컨테이너 삭제"
+
+  docker rm $USING_PORT_ID
 fi
 
 echo "git pull start." &&
 git pull &&
 echo "git pull done." &&
 echo "docker image build" &&
-docker build -t $NAME . &&
+docker build -t $NAME:$VERSION . &&
 echo "docker image build done." &&
 echo "docker container run" &&
 docker run --name $NAME -p $PORT:8080 -e SPRING_DATASOURCE_URL=$DATASOURCE_URL -e SPRING_DATASOURCE_PASSWORD=$DATASOURCE_PASSWORD -e SPRING_DATASOURCE_USERNAME=$DATASOURCE_USERNAME $NAME:$VERSION
