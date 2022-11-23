4858c143bd484fd7f7b36d13fb6fedee978e23ea
gemdoq <gemdoq@gmail.com>
2022-11-23 14:16:49 +0900

    feature(Dockerfile): Dockerfile add
    Docker Build configuration

---
+++ src/main/java/com/example/springjpaexercise/domain/dto/BookResponse.java
@@ -0,0 +1,26 @@
+package com.example.springjpaexercise.domain.dto;
+
+import com.example.springjpaexercise.domain.entity.Book;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookResponse {
+
+    private Long bookId;
+    private String bookName;
+    private String authorName;
+
+    public static BookResponse of(Book book, String authorName) {
+        return BookResponse.builder()
+                .bookId(book.getId())
+                .bookName(book.getName())
+                .authorName(authorName)
+                .build();
+    }
+}
